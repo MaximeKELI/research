56db2aeddc1b0b4d3082aa75a8c8b83704f6e6ac
MaximeKELI <presidentnetero01@gmail.com>
2025-12-25 09:53:04 +0000

    Building

---
+++ research/lib/screens/candidat/candidat_home.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:provider/provider.dart';
+import '../../providers/auth_provider.dart';
+import '../../screens/offres/offres_list_screen.dart';
+import 'candidat_profil_screen.dart';
+import 'candidat_candidatures_screen.dart';
+
+class CandidatHome extends StatefulWidget {
+  const CandidatHome({super.key});
+
+  @override
+  State<CandidatHome> createState() => _CandidatHomeState();
+}
+
+class _CandidatHomeState extends State<CandidatHome> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    const OffresListScreen(),
+    const CandidatCandidaturesScreen(),
+    const CandidatProfilScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Offres',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.description),
+            label: 'Mes candidatures',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profil',
+          ),
+        ],
+      ),
+      appBar: AppBar(
+        title: const Text('JobApp - Candidat'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () async {
+              await Provider.of<AuthProvider>(context, listen: false).logout();
+              if (mounted) {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (_) => const OffresListScreen()),
+                );
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
